4f969e92ddfc7d7bb11204e64fb97360dc363807
David Browne <davidabrowne@gmail.com>
2021-08-11 14:05:01 +1200

    Build / bundle fiddling

--- rollup.config.js
+++ rollup.config.js
@@ -7,20 +7,28 @@ export default {
   output: [
     {
       file: packageJson.main,
-      format: 'cjs', // commonJS
+      format: 'cjs', // Output CommonJS to support node.js v12
       sourcemap: true,
     },
     {
       file: packageJson.module,
-      format: 'esm', // ES Modules
+      format: 'esm', // Output ES Module to take advantage of newer tooling
       sourcemap: true,
     },
   ],
   plugins: [
     // Run source files through tsc prior to bundling.
-    // The typescript plugin inherits config from `tsconfig.json`, which is where most of the juicy details are configured.
+    // The typescript plugin inherits config from `tsconfig.json`.
     // https://github.com/rollup/plugins/blob/master/packages/typescript/README.md
-    typescript({ tsconfig: 'tsconfig.build.json' }),
+    typescript({
+      // Compilation targeting node.js v12 and above as per:
+      // https://github.com/microsoft/TypeScript/wiki/Node-Target-Mapping#recommended-node-tsconfig-settings
+      // but using `module: "ESNext"` which is then bundled by Rollup: https://stackoverflow.com/a/66812779/3175260
+      target: "ES2019",
+      module: "ESNext",
+      outDir: "./dist",
+      exclude: ["__tests__/**/*.ts"],
+    }),
   ],
   // Mark external dependencies to avoid 'Unresolved dependencies' warnings.
   external: Object.keys(packageJson.dependencies),
