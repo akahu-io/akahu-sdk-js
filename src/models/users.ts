e9035752e39a6a4d26d5a1c17a3d485ae6217826
David Browne <davidabrowne@gmail.com>
2023-12-20 15:00:59 +1300

    Fix incorrect user model fields and add better documentation.
    Closes #48

--- src/models/users.ts
+++ src/models/users.ts
@@ -2,11 +2,47 @@
  * User data as returned from the /users/me endpoint
  */
 export type User = {
+  /**
+   * Akahu's unique identifier for this user.
+   */
   _id: string;
-  access_expires_at: string;
-  first_name?: string;
-  last_name?: string;
-  preferred_name?: string;
+  /**
+   * The email address that this user used to register with Akahu.
+   *
+   * This will always be present if your app has the `AKAHU` scope.
+   */
   email?: string;
-  mobile?: string;
+  /**
+   * The user's preferred name, if they have provided it by updating their
+   * profile at https://my.akahu.nz. This will not be available for most users.
+   */
+  preferred_name?: string;
+  /**
+   * The timestamp that the user first granted your app access to their accounts
+   * (i.e. the time at which your user token was issued). Formatted as an ISO
+   * 8601 timestamp.
+   */
+  access_granted_at: string;
+  /**
+   * The user's first name, if they have provided it.
+   *
+   * @deprecated Only present on some legacy users. You probably want
+   * [party](https://developers.akahu.nz/reference/get_parties) data instead,
+   * which is sourced from the user's connected financial institution(s).
+   */
+  first_name?: string;
+  /**
+   * The user's last name, if they have provided it.
+   *
+   * @deprecated Only present on some legacy users. You probably want
+   * [party](https://developers.akahu.nz/reference/get_parties) data instead,
+   * which is sourced from the user's connected financial institution(s).
+   */
+  last_name?: string;
+  /**
+   * @deprecated This field is unused. You probably want
+   * [party](https://developers.akahu.nz/reference/get_parties) data instead,
+   * which is sourced from the user's connected financial institution(s).
+   */
+  mobile?: undefined;
 };
